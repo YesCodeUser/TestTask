e25c0249c0c90abf362e875ba8d988c9c7df6902
NoCodUuser <geney288@gmail.com>
2025-11-30 12:08:08 +0600

    add user serializers

---
+++ users/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password_repeat = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password_repeat', 'first_name', 'last_name', 'surname']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_repeat']:
+            raise serializers.ValidationError("Passwords are different")
+        del attrs['password_repeat']
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'surname', 'is_deleted', 'created_at', 'updated_at']
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'surname']
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.surname = validated_data.get('surname', instance.surname)
+        instance.save()
+        return instance
+
+
